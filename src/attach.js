9685f171846a0bc818641fb960000a4a5fa3fb54
zhangsn <nkzhangsn@gmail.com>
2025-05-08 22:47:46 +0800

    Update attach.js

--- src/attach.js
+++ src/attach.js
@@ -3,42 +3,43 @@
 * @providesModule whatwg-fetch
 */
 
-let interceptors = [];
 
-function interceptor(fetch, ...args) {
-  const reversedInterceptors = interceptors.reduce((array, interceptor) => [interceptor].concat(array), []);
-  let promise = Promise.resolve(args);
-
-  // Register request interceptors
-  reversedInterceptors.forEach(({ request, requestError }) => {
-    if (request || requestError) {
-      promise = promise.then(args => request(...args), requestError);
-    }
-  });
-
-  // Register fetch call
-  promise = promise.then(args => {
-    const request = new Request(...args);
-    return fetch(request).then(response => {
-      response.request = request;
-      return response;
-    }).catch(error => {
-      error.request = request;
-      return Promise.reject(error);
-    });
-  });
-
-  // Register response interceptors
-  reversedInterceptors.forEach(({ response, responseError }) => {
-    if (response || responseError) {
-      promise = promise.then(response, responseError);
-    }
-  });
-
-  return promise;
-}
 
 module.exports = function attach(env) {
+  let interceptors = [];
+
+  function interceptor(fetch, ...args) {
+    const reversedInterceptors = interceptors.reduce((array, interceptor) => [interceptor].concat(array), []);
+    let promise = Promise.resolve(args);
+  
+    // Register request interceptors
+    reversedInterceptors.forEach(({ request, requestError }) => {
+      if (request || requestError) {
+        promise = promise.then(args => request(...args), requestError);
+      }
+    });
+  
+    // Register fetch call
+    promise = promise.then(args => {
+      const request = new Request(...args);
+      return fetch(request).then(response => {
+        response.request = request;
+        return response;
+      }).catch(error => {
+        error.request = request;
+        return Promise.reject(error);
+      });
+    });
+  
+    // Register response interceptors
+    reversedInterceptors.forEach(({ response, responseError }) => {
+      if (response || responseError) {
+        promise = promise.then(response, responseError);
+      }
+    });
+  
+    return promise;
+  }
   // Make sure fetch is available in the given environment
   if (!env.fetch) {
     try {
